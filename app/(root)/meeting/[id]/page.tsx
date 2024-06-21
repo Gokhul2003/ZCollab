0f742b8997db27c73008322ae911442858570766
Gokhul2003 <thegokhul.kooranchi@gmail.com>
2024-06-21 17:57:52 +0530

    more changes

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -7,7 +7,7 @@ import { useParams } from 'next/navigation';
 import { Loader } from 'lucide-react';
 
 import { useGetCallById } from '@/hooks/useGetCallById';
-// import Alert from '@/components/Alert';
+import Alert from '@/components/Alert';
 import MeetingSetup from '@/components/MeetingSetup';
 import MeetingRoom from '@/components/MeetingRoom';
 
@@ -25,10 +25,9 @@ const MeetingPage = () => {
     </p>
   );
 
-  // get more info about custom call type:  https://getstream.io/video/docs/react/guides/configuring-call-types/
-  // const notAllowed = call.type === 'invited' && (!user || !call.state.members.find((m) => m.user.id === user.id));
+  const notAllowed = call.type === 'invited' && (!user || !call.state.members.find((m) => m.user.id === user.id));
 
-  // if (notAllowed) return <Alert title="You are not allowed to join this meeting" />;
+  if (notAllowed) return <Alert title="You are not allowed to join this meeting" />;
 
   return (
     <main className="h-screen w-full">
