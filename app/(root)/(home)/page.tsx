52d68f0d05428ca18f316d4e609676b07f1e67d0
Gokhul2003 <thegokhul.kooranchi@gmail.com>
2024-06-24 08:39:22 +0530

    date and time updated all completed :)

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,8 +2,14 @@ import React from 'react'
 import MeetingTypeList from '@/components/MeetingTypeList';
 const page = () => {
   const now=new Date();
-  const time=now.toLocaleTimeString('en-us',{hour:'2-digit', minute:'2-digit'})
+  // const time=now.toLocaleTimeString('en-us',{hour:'2-digit', minute:'2-digit'})
   const date=(new Intl.DateTimeFormat('en-us',{dateStyle:'full'})).format(now)
+  const options: Intl.DateTimeFormatOptions = { 
+    hour: '2-digit', 
+    minute: '2-digit', 
+    timeZone: 'Asia/Kolkata' 
+  };
+  const timeInIndia = now.toLocaleTimeString('en-IN', options);
   return (
     <section className="flex size-full flex-col gap-5 text-white">
     <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover"/*bg-hero defined in tailwind css config */>
@@ -12,7 +18,7 @@ const page = () => {
         ZCollab is your all-in-one solution 😀..
         </h2>
         <div className="flex flex-col gap-2">
-        <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
+        <h1 className="text-4xl font-extrabold lg:text-7xl">{timeInIndia}</h1>
         <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
         </div>
       </div>
--- components/MeetingModal.tsx
+++ components/MeetingModal.tsx
@@ -33,7 +33,8 @@ const MeetingModal = ({
   buttonIcon,
 }: MeetingModalProps) => {
   return (
-    <Dialog open={isOpen} onOpenChange={onClose}>
+
+    <Dialog  open={isOpen} onOpenChange={onClose} >
       <DialogContent className="flex w-full max-w-[520px] flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white">
         <div className="flex flex-col gap-6">
           {image && (
